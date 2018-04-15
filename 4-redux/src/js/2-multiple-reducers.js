b112e972f7a120c867ec62776cfb36a4313f557e
Justin Hong <jhong2009@gmail.com>
2018-04-14 21:38:59 -0700

    in 2-multiple-reducers.js: added notes

--- 4-redux/src/js/2-multiple-reducers.js
+++ 4-redux/src/js/2-multiple-reducers.js
@@ -1,47 +1,65 @@
+// Step 1: import combineReducers
 import { combineReducers, createStore } from "redux";
 
+// Step 2: create multiple reducers - each reducers has its own file
 // I would live in a separate file
-const userReducer = (state={}, action) => {
-  switch(action.type) {
+// use ES6 to set default state values
+// reducers must return something
+// errors will occur when reducers return nothing
+// actions can trigger side effects with other reducers
+// reducers are independent of each other
+const userReducer = (state = {}, action) => {
+  // do something to change state
+  switch (action.type) {
     case "SET_NAME": {
-      return {...state, name: action.payload};
+      // make sure to destructure everything in state first
+      // that way state will be overridden with new data
+      return { ...state, name: action.payload };
       break;
     }
     case "SET_AGE": {
-      return {...state, age: action.payload};
+      return { ...state, age: action.payload };
       break;
     }
   }
   return state;
-}
+};
 
 // I would live in a separate file
-const tweetsReducer = (state=[], action) => {
-  switch(action.type) {
+const tweetsReducer = (state = [], action) => {
+  // do something to change state
+  switch (action.type) {
     case "ADD_TWEET": {
       return state.concat({
         id: Date.now(), //fake an ID by using a timestamp
-        text: action.payload,
+        text: action.payload
       });
       break;
     }
   }
   return state;
-}
-
+};
+// Step 3: combine all reducers - also a file with setup code
+// define pieces of data to modify
+// define which reducers modify which piece of data
 const reducers = combineReducers({
+  // userReducer receives user data as state and modifies it
   user: userReducer,
+  // tweetsReducer receives tweets data as state and modifies it
   tweets: tweetsReducer
-})
+});
 
-const store = createStore(reducers)
+const store = createStore(reducers);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
-})
+});
 
-store.dispatch({type: "SET_NAME", payload: "Will"})
-store.dispatch({type: "SET_AGE", payload: 35})
-store.dispatch({type: "SET_AGE", payload: 34})
-store.dispatch({type: "ADD_TWEET", payload: "OMG LIKE LOL"})
-store.dispatch({type: "ADD_TWEET", payload: "I am so like seriously like totally like right now"})
+store.dispatch({ type: "SET_NAME", payload: "Will" });
+store.dispatch({ type: "SET_AGE", payload: 35 });
+store.dispatch({ type: "SET_AGE", payload: 34 });
+store.dispatch({ type: "ADD_TWEET", payload: "OMG LIKE LOL" });
+store.dispatch({
+  type: "ADD_TWEET",
+  payload: "I am so like seriously like totally like right now"
+});

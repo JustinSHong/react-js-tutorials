df6966f0323c62a347714de0627040fb1e47f34d
Justin Hong <jhong2009@gmail.com>
2018-04-15 17:23:44 -0700

    in 3-middleware.js: added notes

--- 4-redux/src/js/3-middleware.js
+++ 4-redux/src/js/3-middleware.js
@@ -1,6 +1,7 @@
+// Step 2: import applyMiddleWare
 import { applyMiddleware, createStore } from "redux";
 
-const reducer = (initialState=0, action) => {
+const reducer = (initialState = 0, action) => {
   if (action.type === "INC") {
     return initialState + 1;
   } else if (action.type === "DEC") {
@@ -9,37 +10,36 @@ const reducer = (initialState=0, action) => {
     throw new Error("AHHHH!!");
   }
   return initialState;
-}
-
-const logger = (store) => (next) => (action) => {
+};
+// Step 4: create middleware
+// usually middleware is an npm package
+// logger is an example of middleware function
+const logger = store => next => action => {
   console.log("Logged", action);
   return next(action);
 };
-
-
-const errorHandler = (store) => (next) => (action) => {
+// another middleware function - catches any errors
+const errorHandler = store => next => action => {
   try {
     return next(action);
-  } catch(e) {
+  } catch (e) {
     console.log("ERROR!", e);
   }
 };
-
-const middleware = applyMiddleware(
-  logger,
-  errorHandler
-)
-const store = createStore(reducer, middleware)
+// Step 3: call applyMiddleware
+const middleware = applyMiddleware(logger, errorHandler);
+// Step 1: Add middleware argument to createStore
+const store = createStore(reducer, middleware);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
-})
+});
 
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "MULT"})
-store.dispatch({type: "DEC"})
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "MULT" }); // create an error
+store.dispatch({ type: "DEC" });

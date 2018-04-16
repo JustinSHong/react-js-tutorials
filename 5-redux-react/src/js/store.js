14877579484c8dad024ed193326a6d2b558b820a
Justin Hong <jhong2009@gmail.com>
2018-04-15 18:29:41 -0700

    in store.js: added notes

--- 5-redux-react/src/js/store.js
+++ 5-redux-react/src/js/store.js
@@ -1,11 +1,11 @@
-import { applyMiddleware, createStore } from "redux"
+import { applyMiddleware, createStore } from "redux";
 
-import logger from "redux-logger"
-import thunk from "redux-thunk"
-import promise from "redux-promise-middleware"
-
-import reducer from "./reducers"
-
-const middleware = applyMiddleware(promise(), thunk, logger())
-
-export default createStore(reducer, middleware)
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+// import the combined reducer
+import reducer from "./reducers";
+// in store.js: store redux store, middleware, and the combined reducer
+const middleware = applyMiddleware(promise(), thunk, logger());
+// exporting redux store to be used by a component
+export default createStore(reducer, middleware);

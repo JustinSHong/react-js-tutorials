133b3be7ecb16b71594558d9770a85451a2fcb94
Justin Hong <jhong2009@gmail.com>
2018-04-15 18:32:19 -0700

    in client.js: added notes

--- 5-redux-react/src/js/client.js
+++ 5-redux-react/src/js/client.js
@@ -1,12 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
+import React from "react";
+import ReactDOM from "react-dom";
+// Step 1: import Provider
+import { Provider } from "react-redux";
 
-import Layout from "./components/Layout"
-import store from "./store"
+import Layout from "./components/Layout";
+import store from "./store";
 
-const app = document.getElementById('app')
-
-ReactDOM.render(<Provider store={store}>
-  <Layout />
-</Provider>, app);
+const app = document.getElementById("app");
+// Step 2: wrap top level component with Provider
+// Step 3: inject store as props into Provider
+ReactDOM.render(
+	<Provider store={store}>
+		<Layout />
+	</Provider>,
+	app
+);

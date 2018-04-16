a141c1be8362f5c8dbcb6469b56dd746cf65257f
Justin Hong <jhong2009@gmail.com>
2018-04-15 18:02:23 -0700

    in 4-async-middleware.js: added notes

--- 4-redux/src/js/4-async-middleware.js
+++ 4-redux/src/js/4-async-middleware.js
@@ -1,24 +1,31 @@
 import { applyMiddleware, createStore } from "redux";
 import axios from "axios";
+// Step 1: import logger
 import logger from "redux-logger";
+// Step 3: import thunk middleware
 import thunk from "redux-thunk";
+// Step 6: import promise() middleware to clean up axios promise requests in dispatch
+// promise() recognizes action.payload is a JS Promise object
+// as a result, it auto sends dispatches for you
+// ie. Pending dispatch/action
+// async promises are cleaned up
 import promise from "redux-promise-middleware";
 
 const initialState = {
   fetching: false,
   fetched: false,
   users: [],
-  error: null,
+  error: null
 };
-
-const reducer = (state=initialState, action) => {
+// Step 5: modify reducer to change state based on incoming actions
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_USERS_PENDING": {
-      return {...state, fetching: true}
+      return { ...state, fetching: true };
       break;
     }
     case "FETCH_USERS_REJECTED": {
-      return {...state, fetching: false, error: action.payload}
+      return { ...state, fetching: false, error: action.payload };
       break;
     }
     case "FETCH_USERS_FULFILLED": {
@@ -26,18 +33,39 @@ const reducer = (state=initialState, action) => {
         ...state,
         fetching: false,
         fetched: true,
-        users: action.payload,
-      }
+        users: action.payload
+      };
       break;
     }
   }
-  return state
-}
-
-const middleware = applyMiddleware(promise(), thunk, logger())
-const store = createStore(reducer, middleware)
-
+  return state;
+};
+// Step 4: add thunk to middleware
+const middleware = applyMiddleware(promise(), thunk, logger());
+const store = createStore(reducer, middleware);
+// Step 2: dispatch function with one argument
+// inside dispatch() is a handful of synchronous actions
+// thunk() middleware allows us to do this
 store.dispatch({
+  // dispatch a fetch users action using axios
   type: "FETCH_USERS",
+  // this api can generate an object of users/collection on demand
+  // great for prototyping
   payload: axios.get("http://rest.learncode.academy/api/wstern/users")
-})
+});
+
+// before using promise() from redux-promise-middleware:
+// store.dispatch((dispatch) => {
+//   dispatch({type: "FETCH_USERS_START"})
+//   axios.get("http://rest.learncode.academy/api/wstern/users")
+//     .then((response) => {
+//       dispatch({type: "RECEIVE_USERS", payload: response.data})
+//     })
+//     .cach((err) => {
+//       dispatch({type: "FETCH_USERS_ERROR", payload: err})
+//     })
+// });
+
+
+
+
